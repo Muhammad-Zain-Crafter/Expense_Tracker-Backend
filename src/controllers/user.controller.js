e270ac4cadc16ba1bd4a8c4ed313c879a6ba3b43
Muhammad-Zain-Crafter <zmunawar872@gmail.com>
2026-02-04 04:21:33 -0800

    Implement user authentication methods and error handling utilities

---
+++ src/controllers/user.controller.js
@@ -0,0 +1,26 @@
+import { User } from "../models/user.model.js";
+import accessToken from "../utils/accessToken.js";
+import { ApiError } from "../utils/apiError.js";
+import { ApiResponse } from "../utils/apiResponse.js";
+import { asyncHandler } from "../utils/asyncHandler.js";
+
+const generateAccessandRefreshToken = async (userId) => {
+  try {
+    const user = await User.findById(userId);
+    const accessToken = user.generateAccessToken();
+    const refreshToken = user.generateRefreshToken();
+
+    // save refreshToken in db
+    user.refreshToken = refreshToken;
+    await user.save({ validateBeforeSave: false });
+
+    return { accessToken, refreshToken };
+  } catch (error) {
+    throw new ApiError(
+      500,
+      "something went wrong while generating access and refresh token"
+    );
+  }
+};
+
+
--- src/models/user.model.js
+++ src/models/user.model.js
@@ -19,3 +19,46 @@ const userShema = new Schema({
         minLength: 6
     }
 }, {timestamps: true})
+
+userShema.pre('save', async function () {
+    // Password is hashed only when it changes
+    if (!this.isModified('password')) {
+        return next();
+    }
+    this.password = await bcrypt.hash(this.password, 10);
+    next();
+})
+// Compare password:
+userSchema.methods.isPasswordCorrect = async function (password) {
+  return await bcrypt.compare(password, this.password)
+}
+
+// generate access token:
+userSchema.methods.generateAccessToken = function() {
+    return jwt.sign(
+        {
+            _id: this._id,
+            name: this.name,
+            email: this.email,  
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        {
+            expiresIn: process.env.ACCESS_TOKEN_EXPIRY
+        }
+    )
+}
+
+// Generate refresh token:
+userSchema.methods.generateRefreshToken = function () {
+  return jwt.sign(
+    {
+      _id: this._id
+    },
+    process.env.REFRESH_TOKEN_SECRET,
+    {
+      expiresIn: process.env.REFRESH_TOKEN_EXPIRY
+    }
+  )
+}
+
+export const User = mongoose.model("User", userSchema);
